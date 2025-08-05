793244619774cb5cb0bc852b3ea5bb6612a1b847
Vivek <vivek.dhondge1112@gmail.com>
2025-08-05 23:41:14 +0530

    Add files via upload
    updated the code

--- TakingInputFromUser.py
+++ TakingInputFromUser.py
@@ -1,2 +1,34 @@
-num1=input("enter first number:")
-num2=input("enter second number:")
+# num1=input("enter first number:")
+# num2=input("enter second number:")
+
+# print(type(num1))  #str
+# print(type(num2))  #str
+
+# print(num1+num2)
+
+# #Approach1
+# num1=int(input("enter first number:"))
+# num2=int(input("enter second number:"))
+
+# print(type(num1))  #int
+# print(type(num2))  #int
+
+# print(num1+num2)
+
+
+# #Approach2
+# num1=input("enter first number:")
+# num2=input("enter second number:")
+
+# print(int(num1)+int(num2))
+
+
+#Example
+num1=input("enter first number")
+num2=input("enter second number")
+
+print(type(num1))
+print(type(num2))
+
+print(float(num1)+float(num2))
+
