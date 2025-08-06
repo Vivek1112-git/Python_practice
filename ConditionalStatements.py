c6f0e54fc2b57d3a95e2cba55f4530673707e638
Vivek <vivek.dhondge1112@gmail.com>
2025-08-06 21:34:14 +0530

    Add files via upload

---
+++ ConditionalStatements.py
@@ -0,0 +1,53 @@
+# #example:- print a person is eligible for vote or not
+# #age>=18
+
+# age=20
+# if age>=18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible for vote")   
+
+# #Example
+# if True:
+#     print("true condition") 
+# else:
+#     print("false condition")
+
+
+# #Example. Find a number even/odd. num%2=0
+# num=10
+# if num%2==0:
+#     print("given number is even")
+# else:
+#     print("given number is odd")
+
+
+# #Example. if else in single line  ( ternary operators)
+# num=10
+# print("even number")if 10%2==0 else print("odd number")
+
+
+# #Example. Multiple statement single line
+# a=10
+# {print("hello"),print("python")}if a>=5 else{print("hii"), print("java")}
+
+
+
+#Multiple conditions using elif
+weekno=5
+if weekno==1:
+    print("sunday")
+elif weekno==2:
+    print("monday")
+elif weekno==3:
+    print("tuesday")
+elif weekno==4:
+    print("wensday")
+elif weekno==5:
+    print("thusday")
+elif weekno==6:
+    print("friday")
+elif weekno==7:
+    print("saturday")
+else:
+    print("invalid weekno")
