4536e1ba759ec4f17008a8001436c298668b0e63
Vivek <vivek.dhondge1112@gmail.com>
2025-08-07 23:55:14 +0530

    Add files via upload

---
+++ rangefunction.py
@@ -0,0 +1,10 @@
+print(list(range(10)))
+print(list(range(5,10)))
+
+#print only odd numbers between 1-10
+print(list(range(1,10,2)))
+
+#print only even numbers between 1-10
+print(list(range(0,10,2)))
+
+print(list(range(10,1,-1)))
